a562e13f1099b4e3e3fabb4233d3cc08d58cbbc4
JiaxingQiu <jiaxingq1996@gmail.com>
2025-11-05 19:42:58 -0500

    ready ft

--- revlm/finetune.py
+++ revlm/finetune.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=20, help="Batch size")
     parser.add_argument("--n_iter", type=int, default=10, help="Inner iterations per batch")
     parser.add_argument("--ckpt_dir", type=str, default=None, help="Directory to save checkpoints (overrides config.yaml)")
-    parser.add_argument("--subsample", type=int, default=1000, help="Evaluate on a random subset of this many examples (0=all)")
+    parser.add_argument("--subsample", type=int, default=0, help="Evaluate on a random subset of this many examples (0=all)")
     parser.add_argument("--res_dir", type=str, default=None, help="Result directory (overrides config.yaml if provided)")
     
     args = parser.parse_args()
